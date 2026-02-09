5088d28b83942e8f6d687f8d3e927e44b29a9bfc
Abthente <g344056g@gmail.com>
2026-02-09 15:30:36 +0300

    Test V_1
    - Изменил скрипт перемещения
    - Добавил анимацию
    -Внёс изменения в объект камеры
    - и т.д.

--- UnityProject/Assets/Scripts/Player/Control/PlayerController.cs
+++ UnityProject/Assets/Scripts/Player/Control/PlayerController.cs
@@ -1,35 +1,44 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerController : MonoBehaviour, IControllable
 {
     private InputSystem _inputSystem;
     private Interact _interact;
     private Rigidbody2D _rb;
-    
+    private Animator animator;
+
     [SerializeField] private float _speed;
 
     private void Awake()
     {
         _inputSystem = new InputSystem();
         _inputSystem.Enable();
-        
+
         _interact = GetComponent<Interact>();
-        
+
         _rb = GetComponent<Rigidbody2D>();
     }
-
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        MoveAnimate(ReadMovement());
+    }
     private void FixedUpdate()
     {
         Move(ReadMovement());
     }
-    
+
     private void OnEnable()
     {
         _inputSystem.Gameplay.Interact.performed += Interact;
     }
-    
+
     private void OnDisable()
     {
         _inputSystem.Gameplay.Interact.performed -= Interact;
@@ -54,5 +63,26 @@ public class PlayerController : MonoBehaviour, IControllable
     private void OnDestroy()
     {
         _inputSystem.Disable();
+    }
+
+    private void MoveAnimate(Vector2 _vector)
+    {
+        if ((_vector.x != 0) || (_vector.y != 0))
+        {
+            MoveAnimation.OnMoving(animator);
+            if (_vector.x > 0)
+            {
+                MoveAnimation.Revers(transform, true);
+            }
+            else if (_vector.x < 0)
+            {
+                MoveAnimation.Revers(transform, false);
+            }
+        }
+        else
+        {
+            MoveAnimation.OnStand(animator);
+        }
+
     }
 }
